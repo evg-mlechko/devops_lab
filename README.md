635ea2ac335550824147da781ae3931d17873fcb
evg-mlechko <evg.mlechko@gmail.com>
2019-06-29 14:39:12 +0300

    fixed by comments

--- README.md
+++ README.md
@@ -5,12 +5,19 @@ default usage run next command from directory, where file is placed:
     python pr-stats.py < user_name >
 
 'user_name' - github's user name
+'token' - github's token
 
 App has required option:
 
     1. < user_name >
 
     App has additional options:
-    1. "python pr-stats.py <user_name> --version"
-    2. "python pr-stats.py <user_name> --repo"
-    3. "python pr-stats.py <user_name> (-h |--help)"
+    
+    1. "python pr-stats.py <user_name> (-h |--help)"
+    2. "python pr-stats.py <user_name> --version"  get info about current version
+    3. "python pr-stats.py <user_name> --prnum"    from which pr collect info
+    4. "python pr-stats.py <user_name> --token"    use next token for login
+    5. "python pr-stats.py <user_name> --days"     how long (days) opened pr
+    6. "python pr-stats.py <user_name> --day"      Day of the week opened
+    7. "python pr-stats.py <user_name> --hd"       Hour of the day opened
+    8. "python pr-stats.py <user_name> --who"      User who opened
--- pr-stats.py
+++ pr-stats.py
@@ -7,11 +7,14 @@ import argparse
 ap = argparse.ArgumentParser(description='Easy way to get info about PR:')
 
 ap.add_argument("user", help="insert github username")
-ap.add_argument("--repo", help="insert number of github pull-request")
+ap.add_argument("--prnum", help="from which pr collect info")
 ap.add_argument("--version", help="output version the app",
                 action="store_true")
-# , action="store_true"
-
+ap.add_argument('--token', help="insert token for login", type=str)
+ap.add_argument('--days', help="how long (days) opened pr", action="store_true")
+ap.add_argument('--day', help="Day of the week opened", action="store_true")
+ap.add_argument('--hd', help="Hour of the day opened", action="store_true")
+ap.add_argument('--who', help="User who opened", action="store_true")
 args = ap.parse_args()
 
 USERNAME = args.user
@@ -19,29 +22,30 @@ if args.version:
     print("Version 1.0.0")
     quit()
 
-if args.repo:
-    prNumber = args.repo
+if args.prnum:
+    prNumber = args.prnum
 else:
     prNumber = '55'
 
 user = "evg-mlechko"
-token = 'b8945f269fcc212e085f149af3efe95290b16335'
+token = args.token
 url = 'https://api.github.com/repos/alenaPy/devops_lab/pulls/%s' % prNumber
 pulls = requests.get(url, auth=(user, token))
 pullsjson = pulls.json()
-
 jsondate = pullsjson["created_at"]
 converted_created_at = datetime.datetime.strptime(jsondate,
                                                   "%Y-%m-%dT%H:%M:%SZ")
-
+# Parameters
 days_opened = datetime.datetime.now() - converted_created_at
-print("Numbers of days opened: " + str(days_opened.days))
-
 day_of_the_week = calendar.day_name[converted_created_at.weekday()]
-print("Day of the week opened : " + day_of_the_week)
-
 hour_of_the_day = converted_created_at.hour
-print("Hour of the day opened: " + str(hour_of_the_day))
-
 user_who_opened = pullsjson['user']['login']
-print("User who opened: " + user_who_opened)
+
+if args.days:
+    print("Numbers of days opened: " + str(days_opened.days))
+if args.day:
+    print("Day of the week opened : " + day_of_the_week)
+if args.hd:
+    print("Hour of the day opened: " + str(hour_of_the_day))
+if args.who:
+    print("User who opened: " + user_who_opened)
